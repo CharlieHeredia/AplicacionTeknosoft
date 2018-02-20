f3e5e89f9c27071a1ec430134fb3df231d752d59
alejandro <alejandroheredia95@gmail.com>
2018-02-20 12:49:16 -0600

    Pantalla de carga. Se agrego una pantalla de carga.

---
+++ AplicacionSDKDesktop/Formularios/Splash.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Splash
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Splash))
+        Me.NotifyIconTeknoComAPI = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIconTeknoComAPI
+        '
+        Me.NotifyIconTeknoComAPI.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIconTeknoComAPI.Icon = CType(resources.GetObject("NotifyIconTeknoComAPI.Icon"), System.Drawing.Icon)
+        Me.NotifyIconTeknoComAPI.Text = "TeknoCom API"
+        Me.NotifyIconTeknoComAPI.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AbrirToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(101, 48)
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(100, 22)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(100, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'Splash
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(300, 90)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Splash"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TeknoCom API"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents NotifyIconTeknoComAPI As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents AbrirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+End Class
---
+++ AplicacionSDKDesktop/Formularios/Splash.vb
@@ -0,0 +1,14 @@
+﻿Public Class Splash
+
+    Private Sub Splash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Show()
+        NotifyIconTeknoComAPI.Visible = True
+        Threading.Thread.Sleep(1000)
+        For i As Integer = 100 To i = 0 Step -1
+            Me.Opacity -= 0.01
+            Threading.Thread.Sleep(30)
+        Next
+        Me.ShowInTaskbar = False
+        Me.NotifyIconTeknoComAPI.ShowBalloonTip(100, "TeknoCom API", "Bienvenidos", ToolTipIcon.Info)
+    End Sub
+End Class
